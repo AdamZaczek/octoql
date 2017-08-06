ede14a684dce6424204bea57051966e394daff76
Adam Żaczek <adamzaczekul@gmail.com>
2017-08-06 12:51:11 +0200

    Add Missing Space

--- generators/app/templates/readConfig.js
+++ generators/app/templates/readConfig.js
@@ -1,7 +1,3 @@
 import configFile from './configFile.js';
-const { spawn } = require('child_process');
 
 console.log(configFile.fields);
-
-//const getFields = spawn('babel-node configFile.js')
-//console.log(getFields)
